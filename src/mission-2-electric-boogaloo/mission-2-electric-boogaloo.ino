5802e1bded3cbd0142f7a76752b1b4237b17383a
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2021-04-21 13:55:27 -0400

    Commit

--- src/mission-2-electric-boogaloo/mission-2-electric-boogaloo.ino
+++ src/mission-2-electric-boogaloo/mission-2-electric-boogaloo.ino
@@ -1,20 +1,19 @@
 #define MC_ENABLE 0
 #define MC_DIRECTION 1
 #define SOLENOID 4
-#define START_WAIT 30000 //30 seconds
+#define START_WAIT 2000 //2 seconds
 unsigned long seconds = 1000;
-unsigned long duration = 28;
+unsigned long duration = 26;
 void setup() {
   pinMode(SOLENOID, OUTPUT);
   pinMode(MC_ENABLE, OUTPUT);
   pinMode(MC_DIRECTION, OUTPUT);
-  delay(5000);
-  expandBellows();
-
 }
 
 void loop() {
-
+ expandBellows();
+ delay(START_WAIT);
+ contractBellows();
 }
 
 void expandBellows() {
@@ -23,6 +22,21 @@ void expandBellows() {
   unsigned long starttime = millis();
   unsigned long endtime = starttime;
   digitalWrite(MC_DIRECTION, HIGH);
+  digitalWrite(MC_ENABLE, HIGH);
+  while((endtime - starttime) <= seconds*duration) {
+    endtime = millis();
+  }
+  digitalWrite(MC_ENABLE, LOW);
+  delay(START_WAIT);
+  digitalWrite(SOLENOID, LOW);
+}
+
+void contractBellows() {
+  digitalWrite(SOLENOID, HIGH);
+  delay(START_WAIT);
+  unsigned long starttime = millis();
+  unsigned long endtime = starttime;
+  digitalWrite(MC_DIRECTION, LOW);
   digitalWrite(MC_ENABLE, HIGH);
   while((endtime - starttime) <= seconds*duration) {
     endtime = millis();
