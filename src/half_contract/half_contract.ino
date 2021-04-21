847a4de4a1e801ffb43de8a9168c54fbb1cf5b7f
Asha Bhandarkar <abhandarkar6@gatech.edu>
2021-04-21 00:21:24 -0400

    test code for pool test

---
+++ src/half_contract/half_contract.ino
@@ -0,0 +1,34 @@
+#define MC_ENABLE 0
+#define MC_DIRECTION 1
+#define SOLENOID 4
+#define START_WAIT 30000 //30 seconds
+unsigned long seconds = 1000;
+unsigned long duration = 26;
+void setup() {
+  pinMode(SOLENOID, OUTPUT);
+  pinMode(MC_ENABLE, OUTPUT);
+  pinMode(MC_DIRECTION, OUTPUT);
+
+}
+
+void loop() {
+  delay(5000);
+  contractBellows();
+  delay(5000);
+
+}
+
+void contractBellows() {
+  digitalWrite(SOLENOID, HIGH);
+  delay(START_WAIT);
+  unsigned long starttime = millis();
+  unsigned long endtime = starttime;
+  digitalWrite(MC_DIRECTION, LOW);
+  digitalWrite(MC_ENABLE, HIGH);
+  while((endtime - starttime) <= seconds*duration) {
+    endtime = millis();
+  }
+  digitalWrite(MC_ENABLE, LOW);
+  delay(START_WAIT);
+  digitalWrite(SOLENOID, LOW);
+}
---
+++ src/half_expand/half_expand.ino
@@ -0,0 +1,37 @@
+#define MC_ENABLE 0
+#define MC_DIRECTION 1
+#define SOLENOID 4
+#define START_WAIT 30000 //30 seconds
+unsigned long seconds = 1000;
+unsigned long duration = 26;
+void setup() {
+  pinMode(SOLENOID, OUTPUT);
+  pinMode(MC_ENABLE, OUTPUT);
+  pinMode(MC_DIRECTION, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  expandBellows();
+  delay(5000);
+  
+  
+
+}
+
+void expandBellows() {
+  digitalWrite(SOLENOID, HIGH);
+  delay(START_WAIT);
+  unsigned long starttime = millis();
+  unsigned long endtime = starttime;
+  digitalWrite(MC_DIRECTION, HIGH);
+  digitalWrite(MC_ENABLE, HIGH);
+  while((endtime - starttime) <= seconds*duration) {
+    endtime = millis();
+  }
+  digitalWrite(MC_ENABLE, LOW);
+  delay(START_WAIT);
+  digitalWrite(SOLENOID, LOW);
+}
